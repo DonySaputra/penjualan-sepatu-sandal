78c154836b694794918e37118eab2fcb69bf13c8
DonySaputra <dony90191@gmail.com>
2018-10-13 09:58:35 +0700

    pertama

---
+++ sinarjaya/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'dbpenjualan',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => '0iryFUjtZ7xseSbFEnVSvg9WZaffdp77E3D9RJmv4SPPfyzLHYiF7DXP',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-10-13',
+    'locale' => 'id-ID',
+    'cookie_key' => 'NOCCr7CHnsCdevGpn2gsKWocONXFLJfwO5qSLdC1OENOPRhQogGIo9IU',
+    'cookie_iv' => 'PYbZHs9b',
+    'new_cookie_key' => 'def000000e3744ac5ba0d4767293b75b3cbc2b806f09f22f28d0316e317f39228627ae9910526d7949ae9cd9603593d10c28cf874cfb5dac069815a7ce97926261bbbfdb',
+  ),
+);
